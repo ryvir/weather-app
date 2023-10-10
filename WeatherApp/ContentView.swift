a410fb13a41c6c06b56ead93ca89cda2cd9f4edd
ryv10 <74744128+ryvir@users.noreply.github.com>
2023-10-10 11:37:06 -0700

    Make the app

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var weatherAPIClient = WeatherAPIClient()
+    
     var body: some View {
-        HStack {
-            Text("Weather in Pleasanton")
-                .font(.largeTitle)
-                .lineLimit(nil)
-                .padding()
-                .multilineTextAlignment(.center)
+        VStack(alignment: .center, spacing: 10) {
+            Spacer()
+            if let currentWeather = weatherAPIClient.currentWeather  {
+                HStack(alignment: .center, spacing: 16) {
+                    currentWeather.weatherCode.image
+                        .font(.largeTitle)
+                    Text("\(currentWeather.temperature * (9/5) + 32)º")
+                        .font(.largeTitle)
+                }
+                Text(currentWeather.weatherCode.description)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+            } else {
+                Text("No weather info available yet.\nTap on refresh to fetch new data.\nMake sure you have enabled location permissions for the app.")
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                Button("Refresh", action: {
+                    Task {
+                        await weatherAPIClient.fetchWeather()
+                    }
+                })
+            }
+            Spacer()
+        }
+        .onAppear {
+            Task {
+                await weatherAPIClient.fetchWeather()
+            }
         }
     }
 }
